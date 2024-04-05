ca5200e4121fc1795447895b55eb98cead36c299
Kwasi <kwasi@Kwasis-MacBook-Pro.local>
2024-04-05 22:58:15 +0000

    Bugs fixed 

--- src/components/index.js
+++ src/components/index.js
@@ -1,27 +1,23 @@
 import Navbar from "./Navbar";
-import Billing from "./Billings";
-import Business from "./Business";
-// import Button from "./Button";
+import Billing from "./Billing";
 import CardDeal from "./CardDeal";
+import Business from "./Business";
 import Clients from "./Clients";
 import CTA from "./CTA";
-// import FeedbackCard from "./FeedbackCard";
-import Footer from "./Footer";
-import Hero from "./Hero";
 import Stats from "./Stats";
+import Footer from "./Footer";
 import Testimonials from "./Testimonials";
+import Hero from "./Hero";
 
-export{
-    Navbar,
-    Billing,
-    Business,
-    // Button,
-    CTA,
-    CardDeal,
-    Clients,
-    // FeedbackCard,
-    Footer,
-    Hero,
-    Stats,
-    Testimonials
-}
+export {
+  Navbar,
+  Billing,
+  CardDeal,
+  Business,
+  Clients,
+  CTA,
+  Stats,
+  Footer,
+  Testimonials,
+  Hero,
+};
